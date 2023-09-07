92a3464143bee3075076b38862da155d9bf383a4
mtoan <mtoansecond@gmail.com>
2023-09-07 11:15:38 +0700

    Tao moi folder client

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+import './index.css'
+const App = () => {
+    return (
+      <div className='background'>
+        <h1>Hello world</h1>
+      </div>
+    )
+}
+
+export default App
