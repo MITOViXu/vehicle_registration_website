a8abba6defb6dad136e0e297e1fb7b9f6e73711b
mtoan <mtoansecond@gmail.com>
2023-09-07 11:20:53 +0700

    Add route

---
+++ client/src/routers/Routers.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import Home from '../pages/Home'
+import About from '../pages/About'
+
+const Routers = () => {
+    return (<Routes>
+        <Route path='/' element={<Navigate to ='/home' />} />
+    </Routes>)
+}
+
+export default Routers
