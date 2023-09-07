bb36ba9e794a346e593f0c91fccff72c23fef83a
tUAN21522748 <136876408+tUAN21522748@users.noreply.github.com>
2023-09-07 15:09:03 +0700

    change settings of hardhat.config file

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -2,5 +2,13 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity:{
+    version : "0.8.19",
+    settings:{
+      optimizer:{
+        runs:200,
+        enable:true
+      }
+    }
+  } 
 };
