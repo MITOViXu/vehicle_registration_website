45311ee6b34c0c6f889e0a3021bc7196a88fb544
mtoan <mtoansecond@gmail.com>
2023-09-07 15:41:26 +0700

    upđate phat

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+const CustomButton = ({ title, handleClick }) => {
+    return <button onClick={handleClick}>{title}</button>
+}
 const Navbar = () => {
+    let navigateTitle = 'login'
     const navigate = useNavigate()
+    navigate('login')
+
     return (
         <div className="navbarDiv">
-            <button
-                onClick={() => {
-                    navigate('login')
+            <CustomButton
+                title={navigateTitle == 'login' ? 'tracuu' : 'login'}
+                handleClick={() => {
+                    navigateTitle =
+                        navigateTitle == 'login' ? 'tracuu' : 'login'
+                    
                 }}
-            >
-                Đến Login
-            </button>
+            />
         </div>
     )
 }
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
+    return (
+        <div className="login-container">
+            <div className="login-container-title">
+                <img src="https://giahanxcg.vr.org.vn/vrms-ui/static/img/bg_header.3bc74518.webp" alt="" />
+                <br />
+                <button className='btn_connect'>
+                  Connect to MetaMask
+                </button>
+            </div>
+        </div>
+    )
 }
 
 export default Login
