ec01af911e0431b1267e845ea75eddc596703207
tUAN21522748 <136876408+tUAN21522748@users.noreply.github.com>
2023-09-18 16:03:08 +0700

    fix deploy.js file

--- smart_contract/scripts/deploy.js
+++ smart_contract/scripts/deploy.js
@@ -2,12 +2,8 @@ const hre = require('ethers')
 
 
 async function main() {
-<<<<<<< HEAD
-  const Vehicle = await ethers.getContractFactory("Lock");
-=======
- const Vehicle = await ethers.getContractFactory("Lock");
->>>>>>> e83ef4ad0d4f71409f779373c1bc974470afd8c7
 
+  const Vehicle = await ethers.getContractFactory("Lock");
   // Start deployment, returning a promise that resolves to a contract object
   const _Vehicle = await Vehicle.deploy();
   console.log("Contract address:" + _Vehicle.address)
