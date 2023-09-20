c534e8d83e4c2ed689d89e883f0d6dd5ff72aa6f
tUAN21522748 <136876408+tUAN21522748@users.noreply.github.com>
2023-09-20 21:13:17 +0700

    update client folder (Login component)

---
+++ client/src/components/Connected.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Connected = () => {
+  return (
+    <div>connected</div>
+  )
+}
+
+export default Connected
--- client/src/constant/constant.js
+++ client/src/constant/constant.js
@@ -1,3 +1,261 @@
 //this file include the abi and contract address
-const contractAddress =""
-const abi = ""
+const contractAddress ="0x872b9e9218AeF2f3b129e0bA5590438C572b453A"
+const abi = [
+  {
+    "inputs": [],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "from",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "timestamp",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "internalType": "string",
+        "name": "message",
+        "type": "string"
+      }
+    ],
+    "name": "NewUpdate",
+    "type": "event"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "NumberPlate",
+        "type": "string"
+      }
+    ],
+    "name": "getVehicleInfo",
+    "outputs": [
+      {
+        "components": [
+          {
+            "internalType": "string",
+            "name": "vehicleOwner",
+            "type": "string"
+          },
+          {
+            "internalType": "string",
+            "name": "numberPlate",
+            "type": "string"
+          },
+          {
+            "internalType": "uint8",
+            "name": "yearManufac",
+            "type": "uint8"
+          },
+          {
+            "internalType": "uint8",
+            "name": "lifetimeLimit",
+            "type": "uint8"
+          },
+          {
+            "internalType": "string",
+            "name": "insepectionReportN",
+            "type": "string"
+          },
+          {
+            "internalType": "string",
+            "name": "insepectionValidUntil",
+            "type": "string"
+          },
+          {
+            "internalType": "string",
+            "name": "typeOf",
+            "type": "string"
+          },
+          {
+            "internalType": "string",
+            "name": "mark",
+            "type": "string"
+          },
+          {
+            "internalType": "uint256",
+            "name": "modelCode",
+            "type": "uint256"
+          },
+          {
+            "internalType": "uint256",
+            "name": "chassicNum",
+            "type": "uint256"
+          }
+        ],
+        "internalType": "struct Lock.Vehicle",
+        "name": "",
+        "type": "tuple"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "name": "numberPlates",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "owner",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "NumberPlate",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "VehicleOwner",
+        "type": "string"
+      },
+      {
+        "internalType": "uint8",
+        "name": "YearManufac",
+        "type": "uint8"
+      },
+      {
+        "internalType": "uint8",
+        "name": "LifetimeLimit",
+        "type": "uint8"
+      },
+      {
+        "internalType": "string",
+        "name": "InsepectionReportN",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "InsepectionValidUntil",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "Typeof",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "Mark",
+        "type": "string"
+      },
+      {
+        "internalType": "uint256",
+        "name": "ModelCode",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "ChassicNum",
+        "type": "uint256"
+      }
+    ],
+    "name": "storeVehicle",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "vehicles",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "vehicleOwner",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "numberPlate",
+        "type": "string"
+      },
+      {
+        "internalType": "uint8",
+        "name": "yearManufac",
+        "type": "uint8"
+      },
+      {
+        "internalType": "uint8",
+        "name": "lifetimeLimit",
+        "type": "uint8"
+      },
+      {
+        "internalType": "string",
+        "name": "insepectionReportN",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "insepectionValidUntil",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "typeOf",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "mark",
+        "type": "string"
+      },
+      {
+        "internalType": "uint256",
+        "name": "modelCode",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "chassicNum",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+]
+
+export {contractAddress,abi}
