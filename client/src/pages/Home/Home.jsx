8882f81148f40290276a4a717c717ff7f48d88f5
mtoan <109912739+MITOViXu@users.noreply.github.com>
2023-10-18 21:17:56 +0700

    Sldier

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import './Home.css'
-import '../../components/FindCar/FindCar.jsx'
+import "./Home.css";
+import "../../components/FindCar/FindCar.jsx";
 import { useState, useEffect } from "react";
 import { abi, contractAddress } from "../../constant/constant";
 import { ethers } from "ethers";
 import FindCar from "../../components/FindCar/FindCar.jsx";
-
+import SliderAnimate from "../../components/SliderAnimate/SliderAnimate";
 const Home = (props) => {
   const [provider, setProvider] = useState(null);
   const [vehicleinfor, setVehicleinfor] = useState(null);
@@ -55,100 +55,108 @@ const Home = (props) => {
     }
   };
   return (
-    <div className="login-container-home"  style={{marginTop:"70px"}}>
-     <FindCar Submit={handleSubmit}/>
+    <div>
+      <div className="login-container-home" style={{ marginTop: "70px" }}>
+        <FindCar Submit={handleSubmit} />
 
-      {vehicleinfor == null ? (
-        <div>
-          <h1>
-            <span style={{}}>You didn't search yet</span>
-          </h1>
-        </div>
-      ) : (
-        <div className="information-car">
-          <div class="form-group">
-            <label for="name">Thông tin chủ xe</label>
-            <input
-              type="text"
-              value={vehicleinfor.vehicleOwner}
-              id="1"
-              readOnly
-            />
+        {vehicleinfor == null ? (
+          <div>
+            <h1>
+              <span style={{}}>You didn't search yet</span>
+            </h1>
           </div>
-          <div class="form-group">
-            <label for="name">Biển số xe</label>
-            <input
-              type="text"
-              id="2"
-              value={vehicleinfor.numberPlate}
-              readOnly
-            />
-          </div>
+        ) : (
+          <div className="information-car">
+            <div class="form-group">
+              <label for="name">Thông tin chủ xe</label>
+              <input
+                type="text"
+                value={vehicleinfor.vehicleOwner}
+                id="1"
+                readOnly
+              />
+            </div>
+            <div class="form-group">
+              <label for="name">Biển số xe</label>
+              <input
+                type="text"
+                id="2"
+                value={vehicleinfor.numberPlate}
+                readOnly
+              />
+            </div>
 
-          <div class="form-group">
-            <label for="name">Năm sản xuất</label>
-            <input
-              type="text"
-              id="3"
-              value={vehicleinfor.yearManufac}
-              readOnly
-            />
-          </div>
+            <div class="form-group">
+              <label for="name">Năm sản xuất</label>
+              <input
+                type="text"
+                id="3"
+                value={vehicleinfor.yearManufac}
+                readOnly
+              />
+            </div>
 
-          <div class="form-group">
-            <label for="name">Loại phương tiện</label>
-            <input type="text" id="7" value={vehicleinfor.typeOf} readOnly />
-          </div>
+            <div class="form-group">
+              <label for="name">Loại phương tiện</label>
+              <input type="text" id="7" value={vehicleinfor.typeOf} readOnly />
+            </div>
 
-          <div class="form-group">
-            <label for="name">Nhãn hiệu xe</label>
-            <input type="text" id="8" value={vehicleinfor.mark} readOnly />
-          </div>
+            <div class="form-group">
+              <label for="name">Nhãn hiệu xe</label>
+              <input type="text" id="8" value={vehicleinfor.mark} readOnly />
+            </div>
 
-          <div class="form-group">
-            <label for="name">Số loại</label>
-            <input type="text" id="9" value={vehicleinfor.modelCode} readOnly />
-          </div>
+            <div class="form-group">
+              <label for="name">Số loại</label>
+              <input
+                type="text"
+                id="9"
+                value={vehicleinfor.modelCode}
+                readOnly
+              />
+            </div>
 
-          <div class="form-group">
-            <label for="name">Số khung</label>
-            <input
-              type="text"
-              id="10"
-              value={vehicleinfor.chassicNum}
-              readOnly
-            />
-          </div>
+            <div class="form-group">
+              <label for="name">Số khung</label>
+              <input
+                type="text"
+                id="10"
+                value={vehicleinfor.chassicNum}
+                readOnly
+              />
+            </div>
 
-          <div class="form-group">
-            <label for="name">Niêm Hạn sử dụng</label>
-            <input
-              type="text"
-              id="4"
-              value={vehicleinfor.lifetimeLimit}
-              readOnly
-            />
+            <div class="form-group">
+              <label for="name">Niêm Hạn sử dụng</label>
+              <input
+                type="text"
+                id="4"
+                value={vehicleinfor.lifetimeLimit}
+                readOnly
+              />
+            </div>
+            <div class="form-group">
+              <label for="name">Số phiếu kiểm định</label>
+              <input
+                type="text"
+                id="5"
+                value={vehicleinfor.insepectionReportN}
+                readOnly
+              />
+            </div>
+            <div class="form-group">
+              <label for="name">Hiệu lực đến năm</label>
+              <input
+                type="text"
+                id="6"
+                value={vehicleinfor.insepectionValidUntil}
+                readOnly
+              />
+            </div>
           </div>
-          <div class="form-group">
-            <label for="name">Số phiếu kiểm định</label>
-            <input
-              type="text"
-              id="5"
-              value={vehicleinfor.insepectionReportN}
-              readOnly
-            />
-          </div>
-          <div class="form-group">
-            <label for="name">Hiệu lực đến năm</label>
-            <input
-              type="text"
-              id="6"
-              value={vehicleinfor.insepectionValidUntil}
-              readOnly
-            />
-          </div>
-        </div>
-      )}
+        )}
+      </div>
+      <SliderAnimate />
     </div>
   );
 };
