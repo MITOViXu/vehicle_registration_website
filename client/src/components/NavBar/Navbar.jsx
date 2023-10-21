7548fe39f73224dbcf4e439d7f56d508c1b578f1
Diệp Phi <127822124+Jellyvous@users.noreply.github.com>
2023-10-21 15:14:50 +0700

    update navbar

--- client/src/components/NavBar/Navbar.jsx
+++ client/src/components/NavBar/Navbar.jsx
@@ -23,13 +23,70 @@ const Navbar = () => {
   const toggleMenu = () => menuRef.current.classList.toggle("menu__active");
   return (
     //className="navigation__wrapper d-flex align-items-center justify-content-between"
-
+    
     <div className="main_navbar" style={{position:"fixed", right:"0", left:"0", top:"0", zIndex:"10000"}}>
-      {/* <span className="mobile__menu">
-        <i class="ri-menu-line" onClick={toggleMenu}></i>
-      </span> */}
+
+      <div className="topbar">
+
+        <div className="need-help">
+          <div className="need-help_text">Cần trợ giúp?</div>
+          <div className="icon-container">
+            <img src={'telephone.png'} alt="telephone" className="need-help-icon" />  
+          </div>
+          <div className="need-help-number">1900 1000</div>
+        </div>
+
+        <div className="login-register">
+          <div className="login-nav">
+            <img src={'login.png'} alt="login" className="topbar-icon"/>
+            Login
+          </div>
+          <div className="register-nav">
+            <img src={'user.png'} alt="register" className="topbar-icon"/>
+            Register
+          </div>
+            
+        </div>
+      </div>
+
+        <div className="time-location">
+
+          <div className="website-name">
+
+            <div className="website-logo">
+              <img src={'car.png'} alt="logo" className="logo" />
+            </div>
+
+            Website Đăng Kiểm
+
+          </div>
+
+          <div className="location">
+
+            <img src={'planet-earth.png'} alt="earth" className="logo" />
+            <div>
+              <div className="location-country">Việt Nam</div>
+              <div className="location-city">Hồ Chí Minh</div>
+            </div>
+
+          </div>
+          
+          <div className="time">
+            <img src={'clock.png'} alt="clock" className="logo" />
+            <div>
+              <div className="time-date">Sunday</div>
+              <div className="time-hour">10am-8pm</div>
+            </div>
+          </div>
+
+          <div className="request-call">
+            <button className="request-call-btn">Tư vấn</button>
+          </div>
+        
+        </div>
 
       <div className="navigation" ref={menuRef} onClick={toggleMenu}>
+      
         <div className="menu">
           {navLinks.map((item, index) => (
               <NavLink
@@ -43,16 +100,14 @@ const Navbar = () => {
               </NavLink>
           ))}
         </div>
-      </div>
 
-      <div>
-        <div>
+        <div className="navbar_search">
           <input type="text" placeholder="Search" />
-          <span>
-            <i></i>
-          </span>
         </div>
       </div>
+      
+
+      
     </div>
   );
 };
